246c3a52bb6d86287afd26f9a3635837af80c25d
joseefrodriguesbr <jose.e.f.rodrigues.br@gmail.com>
2025-08-15 14:46:27 -0300

    Initial commit

---
+++ src/main/java/br/inatel/pos/dm111/vfp/ValeFoodPromoManagementApplication.java
@@ -0,0 +1,13 @@
+package br.inatel.pos.dm111.vfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValeFoodPromoManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValeFoodPromoManagementApplication.class, args);
+	}
+
+}
